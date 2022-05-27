28ec6e3ff73c5e04ab7b6479eded7570ee576748
babita971 <babitabisht179@gmail.com>
2022-05-27 14:32:59 +0530

    Added fetch album project

---
+++ fetch_album/android/app/src/main/kotlin/com/example/fetch_album/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fetch_album
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ fetch_album/windowsvsmactest/android/app/src/main/kotlin/com/example/windowsvsmactest/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.windowsvsmactest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
